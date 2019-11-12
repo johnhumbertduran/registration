5cf55c4420c2094a71006623c8a067f5e6ee7a48
johnhumbertduran <32027069+johnhumbertduran@users.noreply.github.com>
2019-11-12 23:52:58 +0800

    2nd commit

---
+++ about.php
@@ -0,0 +1,43 @@
+
+
+
+<?php
+
+    require_once('private/bins/initialize.php');
+
+    include(SHARED_PATH . '/header.php');
+    include(SHARED_PATH . '/connections.php');
+    include(SHARED_PATH . '/slides.php');
+    include(SHARED_PATH . '/login_nav.php');
+
+?>
+
+<script>
+document.getElementById("about").classList.add("active_nav");
+// if (document.title != "About") {
+    document.title += " / About";
+// }
+</script>
+
+<br>
+<center>
+<div class="lorem">
+<p>Lorem ipsum dolor sit amet, cum viris aliquam an, cu iudico atomorum qualisque per. Impetus facilisis ei eam. At mnesarchum disputationi ius, euismod equidem qualisque vis ne, sonet singulis vis cu. At eam soleat iudicabit, ad eam intellegat abhorreant incorrupte.</p>
+
+<p>Cu vel noluisse voluptatibus, ut animal utamur dissentiunt per. Cu petentium definitiones nec, partem everti adipisci an nam. Discere adipisci eleifend cu eos. His te veri quaeque, vis id wisi commodo, ei dicant honestatis nec. Sint postea vim in, et nobis consul vis.</p>
+
+<p>Putant comprehensam mel ad, sint adipisci eu cum, et nam sint nemore offendit. An his saperet denique omittam. Senserit voluptatum no vix, nisl dicunt abhorreant vis ne. Vix facer dolorum argumentum no. Tamquam probatus per at, et laudem nemore persecuti est. Eu vim enim paulo tempor.</p>
+
+<p>Cum vivendo disputando no, qui zril copiosae te. In vel omnes congue fastidii. Eum te facilis corpora, etiam dolorum inciderint te vix. Alii nominavi persequeris vis ex.
+
+<p>Vocent accumsan no vis, ei nec deleniti apeirian. In magna argumentum sadipscing has. Nec ad alii ferri affert, duis ullum audire in est. Sit an natum consequat intellegat, constituto adversarium ex eam, ne ferri voluptaria nec.</p>
+</div>
+</center>
+
+
+
+<?php
+
+    include(SHARED_PATH . '/footer.php');
+
+?>
---
+++ private/bins/shared/login_nav.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-sm bg-success navbar-dark sticky-top">
+  <!-- <a class="navbar-brand" href="#">Logo</a> -->
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" id="home" href="../registration">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" id="about" href="<?php echo url_for('/about'); ?>">About</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" id="contact" href="contact">Contact</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" id="register" href="register">Register</a>
+    </li>
+    <br>
+      <div class="container floating d-flex justify-content-end">
+          <form class="form-inline" action="">
+              <div class="input-group mb-3 float-right">
+                <input type="text" name="user" placeholder="Username" class="form-control mr-sm-2" id="user" autocomplete="off">
+                <input type="password" name="password" placeholder="Password" class="form-control mr-sm-2" id="password">
+                <div class="input-group-append">
+                <button class="btn btn-primary" type="submit">Log In</button>
+                </div>
+              </div>
+          </form>
+      </div>
+    </div>
+  </ul>
+</nav>
